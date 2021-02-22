7ca787da6944ced9361a1e5e782de789f06207ee
aarushthechamp14 <aarush211406@gmail.com>
2021-02-22 17:16:46 +0530

    update

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,29 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
       player.getCount();
       form = new Form()
       form.display();
     }
+  }
+  play(){
+    form.hide();
+    textSize(13);
+    text("Game starts",250,100)
+    player.getPlayerInfo()
+    if(allPlayers !== undefined){
+      var displayPosition = 130
+      displayPosition = displayPosition + 20
+      textSize(15);
+      text(allPlayers[plr].name + ":" + allPlayers[plr].distance,120,displayPosition)
+    }
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50;
+      player.update()
+    }
+  
   }
 }
